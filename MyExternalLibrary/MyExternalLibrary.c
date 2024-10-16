7ab04b2bc2511eb0ef6c3bc58174762be1630c27
Torsten Sommer <torsten.sommer@3ds.com>
2024-10-16 15:10:07 +0200

    Add typedef and NULL checks

--- MyExternalLibrary/MyExternalLibrary.c
+++ MyExternalLibrary/MyExternalLibrary.c
@@ -1,26 +1,54 @@
+#include <stdlib.h>
 #include "MyExternalLibrary.h"
 
 
 double MyExternalFunction(double u, ModelicaUtilityFunctions_t* callbacks) {
 
-	callbacks->ModelicaMessage("Hello, I'm MyExternalFunction()!\n");
+	if (callbacks && callbacks->ModelicaMessage) {
+		callbacks->ModelicaMessage("Hello, I'm MyExternalFunction()!\n");
+	}
 
 	return u + 1;
 }
 
+
+typedef void (*ModelicaMessage_t)(const char* string);
+
+struct MyExternalObject_t {
+
+	ModelicaMessage_t modelicaMessage;
+
+};
+
 void* MyExternalObject_create(ModelicaUtilityFunctions_t* callbacks) {
-	return callbacks;
+
+	if (!callbacks || !callbacks->ModelicaMessage) {
+		return NULL;
+	}
+
+	MyExternalObject_t* instance = (MyExternalObject_t*)malloc(sizeof(MyExternalObject_t));
+
+	if (!instance) {
+		return NULL;
+	}
+
+	instance->modelicaMessage = callbacks->ModelicaMessage;
+
+	return instance;
 }
 
-void MyExternalObject_free(void* externalObject) {
-	// nothing to do
+void MyExternalObject_free(MyExternalObject_t* instance) {
+
+	if (instance) {
+		free((void*)instance);
+	}
 }
 
-double MyExternalObjectFunction(double u, void* externalObject) {
+double MyExternalObjectFunction(double u, MyExternalObject_t* instance) {
 
-	ModelicaUtilityFunctions_t* callbacks = (ModelicaUtilityFunctions_t*)externalObject;
-
-	callbacks->ModelicaMessage("Hello, I'm MyExternalObjectFunction()!\n");
+	if (instance && instance->modelicaMessage) {
+		instance->modelicaMessage("Hello, I'm MyExternalObjectFunction()!\n");
+	}
 
 	return u + 2;
 }
--- MyExternalLibrary/MyExternalLibrary.h
+++ MyExternalLibrary/MyExternalLibrary.h
@@ -7,10 +7,12 @@
     #define EXPORT __attribute__((visibility("default")))
 #endif
 
+typedef struct MyExternalObject_t MyExternalObject_t;
+
 EXPORT double MyExternalFunction(double u, ModelicaUtilityFunctions_t* callbacks);
 
 EXPORT void* MyExternalObject_create(ModelicaUtilityFunctions_t* callbacks);
 
-EXPORT void MyExternalObject_free(void* externalObject);
+EXPORT void MyExternalObject_free(MyExternalObject_t* externalObject);
 
-EXPORT double MyExternalObjectFunction(double u, void* externalObject);
+EXPORT double MyExternalObjectFunction(double u, MyExternalObject_t* externalObject);
